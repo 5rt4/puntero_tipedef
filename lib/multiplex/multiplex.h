f219761d70a0c940f0835e8eb8e2c575b662e92a
khalil escudero <escuderokhalil@gmail.com>
2026-06-10 16:50:45 -0300

    lpm

---
+++ lib/multiplex/multiplex.h
@@ -0,0 +1,20 @@
+#ifndef MULTIPLEX_H
+#define MULTIPLEX_H
+#include <stdint.h>
+#include "stm32f103xb.h"
+typedef struct {
+GPIO_TypeDef* puerto;   //Qué puerto es
+unsigned int pin;       //Número de pin
+unsigned int config;    //Configuracion del pin. 0:Salida;1:Entrada Flotante;2:Entrada PU;3:entrada PD
+unsigned int estPin;    //Estado del pin
+} configuracion_t;
+void gpio_config(configuracion_t*);
+void gpio_on_off(configuracion_t*);
+int gpio_estado(configuracion_t*);
+
+GPIO_TypeDef* puerto;   //Qué puerto es
+unsigned int pin;       //Número de pin
+unsigned int config;    //Configuracion del pin. 0:Salida;1:Entrada Flotante;2:Entrada PU;3:entrada PD
+unsigned int estPin;    //Estado del pin
+} configuracion_t;
+#endif
---
+++ src/main2.c
@@ -0,0 +1,1 @@
+. Realizar mediante estructura de tipos de datos una librería para un multiplexor 3 a 1 con sus entradas selectoras
